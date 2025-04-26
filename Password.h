de017d58eba5867c78115137d2a47336567e4a0c
Galina <djngalja@gmail.com>
2025-04-26 12:29:09 +0300

    Update Password.h

--- Password.h
+++ Password.h
@@ -6,10 +6,10 @@
 #include <unordered_set>
 
 class Password {
-private:
+    public:
   std::string m_password;
   int m_count_special_chars;
-  int m_digits;
+  bool m_digit;
   int m_lower_case;
   int m_upper_case;
   std::size_t m_min_pattern_len;
@@ -17,7 +17,6 @@ private:
   std::string m_pattern_string;
   int m_score;
 
-  int ContainsDigits();
   int ContainsUpperCase();
   int ContainsLowerCase();
   int CountSpecialChars();
@@ -26,9 +25,11 @@ private:
   void FindRepeatingPairs();
   void FindAbcPatterns(bool backwards = false);
 
-public:
+
   Password(const std::string& password, std::size_t min_pattern_len = 4);
   friend class Table;
 };
+
+int has_digit(const std::string&);
 
 #endif // PASSWORD_H
