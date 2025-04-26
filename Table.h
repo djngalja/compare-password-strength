9b1406221101f6b3a439d055fd271edd0eef36b3
Galina <djngalja@gmail.com>
2025-04-26 12:28:32 +0300

    Update Table.h

--- Table.h
+++ Table.h
@@ -1,37 +1,38 @@
 #ifndef TABLE_H
 #define TABLE_H
 
-#include <algorithm>
 #include <iostream>
+#include <string>
+#include <vector>
 #include <sstream>
-#include "Password.h"
+
+struct Column {
+    std::string header;
+    std::size_t width;
+};
 
 class Table {
-private:
-  const std::vector<std::string> m_headers = {
-    "Score",
-    "Password",
-    "Length",
-    "SP Chars",
-    "Digits",
-    "LCase",
-    "UCase",
-    "Patterns"
-  };
-  std::vector<std::string> m_longest_words;
-  std::vector<Password> m_passwords;
-
-  void SplitStringIntoPasswords(const std::string& str);
-  void MakeLongestWordVector();
-  template<typename T> std::string MakeCell(
-    T current_word, const std::string& longest_word, bool contains = false
-  );
-  void PrintHeaders();
-  void PrintBorder(char c = '=');
-
+    std::vector<Column> columns {};
+    int table_width {};
+    std::string border {};
+    std::vector<std::vector<std::string>> rows {};
+    const char hor_border_style {'-'};
+    const char vert_border_style {'|'};
+    const char edge_style {'+'};
+    std::vector<std::string> temp_vec {};
 public:
-  Table(const std::string& str);
-  void PrintTable();
+    void add_column(const std::string&, std::size_t);
+    template<typename T, typename... Types>
+    void add_row(T first, Types... tail) {
+        std::ostringstream oss;
+        oss << first;
+        temp_vec.push_back(oss.str());
+        add_row(tail...);
+    }
+    void add_row();
+    void print_border() const;
+    void print_headers() const;
+    void print_rows();
 };
 
 #endif // TABLE_H
