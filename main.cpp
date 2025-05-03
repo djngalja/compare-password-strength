1009a96bc9912f5ac91452100231b4b9f52c70aa
Galina <djngalja@gmail.com>
2025-05-04 00:50:25 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -4,26 +4,34 @@
 
 int main() {
     std::vector<pswd::Password> input = get_input();
+
     std::sort(input.begin(), input.end(),
-              [](pswd::Password a, pswd::Password b){ return a.get_score() > b.get_score(); });
+              [](pswd::Password a, pswd::Password b)
+              { return a.get_score() > b.get_score(); });
+
     Table tb;
-    tb.add_column("Score", 5);
-    tb.add_column("Password", 20);
-    tb.add_column("Length", 5);
-    tb.add_column("SP Chars", 5);
-    tb.add_column("Digits", 5);
-    tb.add_column("LCase", 5);
-    tb.add_column("UCase", 5);
-    tb.add_column("Patterns", 30);
+
+    tb.add_column("Score");
+    tb.add_column("Password");
+    tb.add_column("Length");
+    tb.add_column("SP Chars");
+    tb.add_column("Digits");
+    tb.add_column("LCase");
+    tb.add_column("UCase");
+    tb.add_column("Patterns");
+
     for (const auto& pas : input) {
         tb.add_row(pas.get_score(), pas.get_pswd(), pas.get_pswd().size(),
                    pas.get_sp_chars(), pas.get_digit(), pas.get_l_case(),
                    pas.get_u_case(), pas.get_patterns());
     }
+
+    std::cout << '\n';
     tb.print_border();
     tb.print_headers();
     tb.print_border();
     tb.print_rows();
     tb.print_border();
+
     return 0;
 }
