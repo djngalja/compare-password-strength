8bccaa2483476d4d03244ef6aa6e3b65b9527328
Galina <djngalja@gmail.com>
2025-05-04 00:48:41 +0300

    Update Table.h

--- Table.h
+++ Table.h
@@ -2,9 +2,9 @@
 #define TABLE_H
 
 #include <iostream>
+#include <sstream>
 #include <string>
 #include <vector>
-#include <sstream>
 
 struct Column {
     std::string header;
@@ -12,16 +12,9 @@ struct Column {
 };
 
 class Table {
-    std::vector<Column> columns {};
-    int table_width {};
-    std::string border {};
-    std::vector<std::vector<std::string>> rows {};
-    const char hor_border_style {'-'};
-    const char vert_border_style {'|'};
-    const char edge_style {'+'};
-    std::vector<std::string> temp_vec {};
 public:
-    void add_column(const std::string&, std::size_t);
+    void add_column(const std::string&);
+
     template<typename T, typename... Types>
     void add_row(T first, Types... tail) {
         std::ostringstream oss;
@@ -29,10 +22,29 @@ public:
         temp_vec.push_back(oss.str());
         add_row(tail...);
     }
+
+    template<typename... Types>
+    void add_row(bool first, Types... tail) {
+        if (first) { temp_vec.push_back("+"); }
+        else { temp_vec.push_back("-"); }
+        add_row(tail...);
+    }
+
     void add_row();
+
     void print_border() const;
     void print_headers() const;
-    void print_rows();
+    void print_rows() const;
+private:
+    std::vector<Column> columns {};
+    std::vector<std::vector<std::string>> rows {};
+
+    const char hor_border_style {'='};
+    const char vert_border_style {'|'};
+    const char edge_style {'+'};
+    const int margin {1}; // margin width in chars
+
+    std::vector<std::string> temp_vec {};
 };
 
 #endif // TABLE_H
